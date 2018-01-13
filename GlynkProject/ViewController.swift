92441ab7e456a1ab9db853de8e07ec69a2d27182
proretina <kashee.nitd2016@gmail.com>
2018-01-13 13:06:52 +0530

    Initial Commit

---
+++ GlynkProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GlynkProject
+//
+//  Created by Pro Retina on 13/01/18.
+//  Copyright © 2018 Pro Retina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
